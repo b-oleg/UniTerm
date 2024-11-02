ed1de2d6b4bff6620e9b820ddda8c02aecd8e5b0
Oleg <obolshakov@mail.ru>
2024-11-02 16:40:40 +0300

    v0.4

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,12 @@
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
+    QCoreApplication::setOrganizationName(APP_NAME);
+    QCoreApplication::setApplicationName(APP_NAME);
+    QCoreApplication::setApplicationVersion(APP_VERSION);
+
     QApplication a(argc, argv);
-
-    QCoreApplication::setOrganizationName("IAP RAS");
-    QCoreApplication::setApplicationName("UniTerm");
-    QCoreApplication::setApplicationVersion("0.4");
-
     MainWindow w;
-    //w.setWindowTitle(QString("%1 v%2").arg(QCoreApplication::applicationName(), QCoreApplication::applicationVersion()));
     w.show();
     return a.exec();
 }
